5f512c327a6c31d28dad6fc03ad5793026f8b77a
Andrew Ross <andrew.ross.mail@gmail.com>
2023-01-29 01:56:44 -0500

    First commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+const MAGIC: &str = r#"
+                            Y                       _
+                            YiL                   ,```.
+                            Yii;      WOW       .; .;;`.
+                            YY;ii._           ,;`.;;;; :
+                            iiYYYYYYiiiii;;;;i` ;;::;;;;
+                        _.;YYYYYYiiiiiiYYYii  .;;.   ;;;
+                     .YYYYYYYYYYiiYYYYYYYYYYYYii;`  ;;;;
+   VERY CODE       .YYYYYYY$$YYiiYY$$$$iiiYYYYYY;.ii;`..
+                  :YYY$!.  TYiiYY$$$$$YYYYYYYiiYYYYiYYii.
+                  Y$MM$:   :YYYYYY$!"``"4YYYYYiiiYYYYiiYY.
+               `. :MM$$b.,dYY$$Yii" :'   :YYYYllYiiYYYiYY
+            _.._ :`$MM$!YYYYYYYYYii,.__.diii$$YYYYYYYYYYY
+            .,._ $b`P`     "4$$$$$iiiiiiii$$$$YY$$$$$$YiY;
+               `,.`$:       :$$$$$$$$$YYYYY$$$$$$$$$YYiiYYL
+                "`;$$.    .;PPb$`.,.``T$$YY$$$$YYYYYYiiiYYU:
+              , ;$P$;;: ;;;;i$y$"!Y$$$B;$$$Y$YY$$YYYiiiYYiYY      MUCH HAX
+                $Fi$$ .. ``:iii.`-";YYYYY$$YY$$$$$YYYiiYiYYY
+                :Y$$rb ````  `_..;;i;YYY$YY$$$$$$$YYYYYYiYY:
+                 :$$$$$i;;iiiiidYYYYYYYYY$$$$$$YYYYYYYiiYYYY.
+                  `$$$$$$$YYYYYYYYYYYY$$$$$$YYYYYYYYiiiYYYYYY
+                  .i!$$$$$$YYYYYYYY$$$$$$YYY$$YYiiiiiiYYYYYYY
+                 :YYiii$$$$$$$YYYYYY$$$$YY$$$$YYiiiiiYYYYYYi'
+"#;
+
+fn main() {
+    println!("{}", MAGIC);
+}
